1de3925a242deb02af0052c243445978b2aaaa44
Hamid <mail.hamid.hoseini@gmail.com>
2022-12-27 23:44:35 -0800

    Application init Add SiteNavigation component

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary h-max">
+    <SiteNavigation />
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from "vue-router";
+import SiteNavigation from "./components/SiteNavigation.vue";
+</script>
+
+<style src="./assets/tailwind.css"></style>
