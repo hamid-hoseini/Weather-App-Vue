993428bfc75808ee96a52b72bce2cfd1dc7a4399
Hamid <mail.hamid.hoseini@gmail.com>
2023-01-11 17:57:43 -0800

    Add AsyncCityView Component and get weather API Add current local date & time Cal hourly weather offset Add AsynCityView component into cityview

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>
-
-  </div>
+  <Suspense>
+    <div>
+      <AsyncCityView />
+    </div>
+    <template>
+      <p>loading...</p>
+    </template>
+  </Suspense>
 </template>
 
 <script setup>
+import AsyncCityView from '../components/AsyncCityView.vue';
+
 
 </script>
