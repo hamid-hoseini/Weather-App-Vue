3fd683663f6cf8ab842301b8d3876aed7a3a8372
Hamid <mail.hamid.hoseini@gmail.com>
2023-01-21 19:24:49 -0800

    Add cityViewSkeleton component for card view

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -5,13 +5,14 @@
     <Suspense>
       <AsyncCityView />
       <template #fallback>
-        <p>Loading...</p>
+        <CityViewSkeleton />
       </template>
     </Suspense>
   </div>
 </template>
 <script setup>
 import AsyncCityView from '../components/AsyncCityView.vue';
+import CityViewSkeleton from '../components/CityViewSkeleton.vue';
 
 
 </script>
